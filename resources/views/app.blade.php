b0906048aa502cdec7957b9c1df1a672a7c32f3a
kyoleht <rafaelferreiradasilva1812@gmail.com>
2024-05-18 17:17:26 -0300

    Projeto Documentação Laravel

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+// resources/views/layouts/app.blade.php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Quickstart - Basic</title>
+
+    <!--- CSS And JavaScript -->
+</head>
+<body>
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <!-- navbar contents -->
+        </nav>
+    </div>
+
+    @yield('content')
+
+</body>
+</html>
